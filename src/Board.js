27e1dd40d5dde0f91109828363d03a686d658e42
siningtong <siningtong@gmail.com>
2020-01-03 06:54:59 +0000

    determine whether win the game

--- src/Board.js
+++ src/Board.js
@@ -85,13 +85,16 @@ class Board extends Component {
 
     // win when every cell is turned off
     // TODO: determine is the game has been won
-    let hasWon = false;
+    let hasWon = board.every(row => row.every(cell => !cell));
     this.setState({ board, hasWon });
   }
 
   /** Render game board or winning message. */
 
   render() {
+    if (this.state.hsaWon) {
+      return <h1>You Won!</h1>;
+    }
     let tblBoard = [];
     for (let y = 0; y < this.props.nrows; y++) {
       let row = [];
